3536b485584f10e34ccd04251fdd8f3f70a43b6c
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2025-11-06 10:21:28 -0500

    After testing

--- Client.cs
+++ Client.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace JoshuaProjectClient
